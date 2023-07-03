97518da27fa54bbd7eb075f5360601feecc1834f
Kushal Nepali <kushal10nepali@gmail.com>
2023-07-03 21:19:44 +0545

    Footer added.

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "../components/Layout/Header.jsx"
+import Footer from "../components/Layout/Footer.jsx"
 
 const Homepage = () => {
   return (
     <div>
         <Header />
+        <Footer/>
     </div>
   )
 }
