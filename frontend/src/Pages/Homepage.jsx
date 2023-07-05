978a4ab4f85bee7af6b9805c6d91f371620c19ef
Kushal Nepali <kushal10nepali@gmail.com>
2023-07-05 22:00:26 +0545

    Hero section added .

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "../components/Layout/Header.jsx"
 import Footer from "../components/Layout/Footer.jsx"
+import Hero from "../components/Layout/Hero.jsx"
 
 const Homepage = () => {
   return (
     <div>
         <Header />
+        <Hero/>
         <Footer/>
     </div>
   )
