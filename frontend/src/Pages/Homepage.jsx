721583ba96ac5c5e5e62409bea1684c42ec67038
Kushal Nepali <kushal10nepali@gmail.com>
2023-07-06 22:47:38 +0545

    Products section initialized.

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from "../components/Layout/Header.jsx"
 import Footer from "../components/Layout/Footer.jsx"
 import Hero from "../components/Layout/Hero.jsx"
+import Products from "../components/Layout/Products.jsx"
 
 const Homepage = () => {
   return (
     <div>
         <Header />
         <Hero/>
+        <Products/>
         <Footer/>
     </div>
   )
