7bb484ef67c7f54c701ce075aead583e6c83e91f
Kushal Nepali <kushal10nepali@gmail.com>
2023-07-13 22:36:52 +0545

    Services section added .

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -4,12 +4,14 @@ import Footer from "../components/Layout/Footer.jsx"
 import Hero from "../components/Layout/Hero.jsx"
 import Products from "../components/Layout/Products.jsx"
 import ContactUs from "../components/Layout/ContactUs.jsx"
+import Services from "../components/Layout/Services.jsx"
 
 const Homepage = () => {
   return (
     <div>
         <Header />
         <Hero/>
+        <Services/>
         <Products/>
         <ContactUs/>
         <Footer/>
